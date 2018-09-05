1546fddc0e57b11ce8ba5b68c0912f5771255652
R <R@lurongdeiMac.local>
2018-09-05 14:19:40 +0800

    Initial commit

---
+++ Example/XXLib/BPPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BPPAppDelegate.h
+//  XXLib
+//
+//  Created by baopanpan on 09/05/2018.
+//  Copyright (c) 2018 baopanpan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BPPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XXLib/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XXLib
+//
+//  Created by baopanpan on 09/05/2018.
+//  Copyright (c) 2018 baopanpan. All rights reserved.
+//
+
+@import UIKit;
+#import "BPPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BPPAppDelegate class]));
+    }
+}
